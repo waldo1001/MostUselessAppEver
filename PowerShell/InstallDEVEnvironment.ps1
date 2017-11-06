c51bf24acee991ba1c3e49017714dace437b54aa
waldo1001 <github@waldo.be>
2017-11-06 22:08:28 +0100

    Create Guid Demo

--- PowerShell/InstallDEVEnvironment.ps1
+++ PowerShell/InstallDEVEnvironment.ps1
@@ -1,1 +1,56 @@
-﻿$password = 'waldo1234'New-CSideDevContainer `    -containerName 'DEVServer' `    -devImageName navdocker.azurecr.io/dynamics-nav:devpreview `    -licenseFile 'https://www.dropbox.com/s/ikizvkpl0pgbwf1/2017%20DEV%20License%20CRS.flf?dl=1' `    -vmAdminUsername admin `    -adminPassword (convertTo-SecureString -String $password -AsPlainText -Force) `    -memoryLimit 3G `    -updateHosts `    -auth NavUserPassword `    -Verbose New-CSideDevContainer `    -containerName 'TESTServer' `    -devImageName navdocker.azurecr.io/dynamics-nav:devpreview `    -licenseFile 'https://www.dropbox.com/s/ikizvkpl0pgbwf1/2017%20DEV%20License%20CRS.flf?dl=1' `    -vmAdminUsername admin `    -adminPassword (convertTo-SecureString -String $password -AsPlainText -Force) `    -memoryLimit 3G `    -updateHosts `    -auth NavUserPassword `    -Verbose
+﻿$password = 'waldo1234'
+
+<# New-CSideDevContainer `
+    -containerName 'DEVServer' `
+    -devImageName navdocker.azurecr.io/dynamics-nav:devpreview `
+    -licenseFile 'https://www.dropbox.com/s/ikizvkpl0pgbwf1/2017%20DEV%20License%20CRS.flf?dl=1' `
+    -vmAdminUsername admin `
+    -adminPassword (convertTo-SecureString -String $password -AsPlainText -Force) `
+    -memoryLimit 2G `
+    -updateHosts `
+    -auth NavUserPassword `
+    -accept_eula `
+    -additionalParameters @('--publish 1433:1433') `
+    -Verbose  #>
+
+
+New-NavContainer `
+    -accept_eula `
+    -containerName 'DEVServer' `
+    -imageName navdocker.azurecr.io/dynamics-nav:devpreview `
+    -licenseFile 'https://www.dropbox.com/s/ikizvkpl0pgbwf1/2017%20DEV%20License%20CRS.flf?dl=1' `
+    -vmAdminUsername admin `
+    -adminPassword (convertTo-SecureString -String $password -AsPlainText -Force) `
+    -memoryLimit 2G `
+    -updateHosts `
+    -auth NavUserPassword `
+    -includeCSide `
+    -Verbose `
+    #-additionalParameters @('--publish 1433:1433')
+
+<# New-CSideDevContainer `
+    -containerName 'TESTServer' `
+    -devImageName navdocker.azurecr.io/dynamics-nav:devpreview `
+    -licenseFile 'https://www.dropbox.com/s/ikizvkpl0pgbwf1/2017%20DEV%20License%20CRS.flf?dl=1' `
+    -vmAdminUsername admin `
+    -adminPassword (convertTo-SecureString -String $password -AsPlainText -Force) `
+    -memoryLimit 2G `
+    -updateHosts `
+    -auth NavUserPassword `
+    -accept_eula `
+    -additionalParameters @('--publish 1433:1433') `
+    -Verbose #>
+
+New-NavContainer `
+    -accept_eula `
+    -containerName 'TESTServer' `
+    -imageName navdocker.azurecr.io/dynamics-nav:devpreview `
+    -licenseFile 'https://www.dropbox.com/s/ikizvkpl0pgbwf1/2017%20DEV%20License%20CRS.flf?dl=1' `
+    -vmAdminUsername admin `
+    -adminPassword (convertTo-SecureString -String $password -AsPlainText -Force) `
+    -memoryLimit 2G `
+    -updateHosts `
+    -auth NavUserPassword `
+    -includeCSide `
+    -Verbose `
+    #-additionalParameters @('--publish 1433:1433')
---
+++ README.md
@@ -0,0 +1,7 @@
+# This app has absolutely no use
+This app is completely useless and is especially designed to be used in demo-purposes.
+## Bad design
+The app has a bad design "by design".  During the demo, it will be changed in many forms.
+### This still makes sense
+
+####This somewhat less
