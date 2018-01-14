8c5370852ffea83b2ed4c2988f1fc7ecf5ce62a0
waldo1001 <github@waldo.be>
2018-01-14 11:57:32 +0100

    Enable for local DEV environment

--- PowerShell/Docker_SetUpDEVEnvironment.ps1
+++ PowerShell/Docker_SetUpDEVEnvironment.ps1
@@ -1,27 +1,51 @@
-﻿. '.\PowerShell\Settings.ps1'
+﻿<#
+Enter-PSSession -Authentication Basic -Credential Administrator -ComputerName WaldoCoreVM 
+enter-pssession waldocorevm
+#>
 
-New-NavContainer `
-    -accept_eula `
-    -containerName $DEVServer `
-    -imageName $DockerImage `
-    -licenseFile $LicenseFile `
-    -memoryLimit $MemoryDEVServer `
-    -updateHosts `
-    -auth NavUserPassword `
-    -includeCSide `
-    -Verbose `
-    -Credential $Credential 
+. '.\PowerShell\Settings.ps1'
 
-New-NavContainer `
+Invoke-Command -ComputerName $DockerHost -ScriptBlock {
+    param(
+        $DEVServer,$IPDEVServer,$DockerImage,$LicenseFile,$MemoryDEVServer,[System.Management.Automation.PSCredential] $Credential
+    )
+
+    New-NavContainer `
+        -accept_eula `
+        -containerName $DEVServer `
+        -imageName $DockerImage `
+        -licenseFile $LicenseFile `
+        -alwaysPull `
+        -doNotExportObjectsToText `
+        -additionalParameters @("--network=tlan","--ip $IPDEVServer") `
+        -memoryLimit $MemoryDEVServer `
+        -updateHosts `
+        -auth NavUserPassword `
+        -includeCSide `
+        -Verbose `
+        -Credential $Credential 
+} -ArgumentList $DEVServer,$IPDEVServer,$DockerImage,$LicenseFile,$MemoryDEVServer,$Credential
+
+
+Invoke-Command -ComputerName $DockerHost -ScriptBlock {
+    param(
+        $TESTServer,$IPTESTServer,$DockerImage,$LicenseFile,$MemoryTESTServer,[System.Management.Automation.PSCredential] $Credential
+    )
+
+    New-NavContainer `
     -accept_eula `
     -containerName $TESTServer `
     -imageName $DockerImage `
     -licenseFile $LicenseFile `
+    -alwaysPull `
+    -doNotExportObjectsToText `
+    -additionalParameters @("--network=tlan","--ip $IPTESTServer") `
     -memoryLimit $MemoryTESTServer `
     -updateHosts `
     -auth NavUserPassword `
     -includeCSide `
     -Verbose `
     -Credential $Credential 
+} -ArgumentList $TESTServer,$IPTESTServer,$DockerImage,$LicenseFile,$MemoryTESTServer,$Credential
 
 Start-Process "http://$($DEVServer):8080/"
--- PowerShell/Settings.ps1
+++ PowerShell/Settings.ps1
@@ -1,13 +1,17 @@
 $AppJSON = Get-ObjectFromJSON -Path '.\app.json'
 $LaunchJSON = Get-ObjectFromJSON -Path '.\.vscode\launch.json'
 
+$DockerHost = 'waldocorevm'
+
 $DockerImage = 'microsoft/dynamics-nav:devpreview'
 
 $DEVServer = 'devsrv'
+$IPDEVServer = '172.21.31.8'
 $TESTServer = 'testsrv'
+$IPTESTServer = '172.21.31.9'
 $TESTDatabaseServer = "$TESTServer\SQLEXPRESS"
 $DatabaseName = 'CRONUS'
-$LicenseFile = "C:\Demo\license.flf"
+$LicenseFile = "https://www.dropbox.com/s/8r85nc2oq5r1mal/CRS%20NAV2018%20DEV%20%20License.flf?dl=1"
 $MemoryDEVServer = '3G'
 $MemoryTESTServer = '3G'
 
